663188bc1b678bb8ec1ff5deefa04bc284933b7d
Fahad Subzwari <fahadsubzwari924@gmail.com>
2024-01-15 17:54:00 +0500

    cwc-47: added returned orders count in widget on dashboard (#23)

--- src/app/modules/dashboard/model/dashboard.model.ts
+++ src/app/modules/dashboard/model/dashboard.model.ts
@@ -5,20 +5,44 @@ export class DashboardStatsModel {
   totalPendingOrders: DashboardStatsPropertyModel;
   totalDispatchedOrders: DashboardStatsPropertyModel;
   totalDeliveredOrders: DashboardStatsPropertyModel;
+  totalReturnedOrders: DashboardStatsPropertyModel;
   repeatedCustomerPercentage: DashboardStatsPropertyModel;
 
   constructor(data: any) {
-    this.totalCustomers = new DashboardStatsPropertyModel({ name: 'Customers', count: data?.totalCustomers });
-    this.totalProducts = new DashboardStatsPropertyModel({ name: 'Products', count: data?.totalProducts });
-    this.totalOrders = new DashboardStatsPropertyModel({ name: 'Orders', count: data?.totalOrders });
-    this.totalPendingOrders = new DashboardStatsPropertyModel({ name: 'Pending Order', count: data?.totalPendingOrders });
-    this.totalDispatchedOrders = new DashboardStatsPropertyModel({ name: 'Dispatched Orders', count: data?.totalDispatchedOrders });
-    this.totalDeliveredOrders = new DashboardStatsPropertyModel({ name: 'Delivered Orders', count: data?.totalDeliveredOrders });
-    this.repeatedCustomerPercentage = new DashboardStatsPropertyModel({ name: 'Repeated Customer', count: data?.repeatedCustomerPercentage });
-
+    this.totalCustomers = new DashboardStatsPropertyModel({
+      name: 'Customers',
+      count: data?.totalCustomers,
+    });
+    this.totalProducts = new DashboardStatsPropertyModel({
+      name: 'Products',
+      count: data?.totalProducts,
+    });
+    this.totalOrders = new DashboardStatsPropertyModel({
+      name: 'Orders',
+      count: data?.totalOrders,
+    });
+    this.totalPendingOrders = new DashboardStatsPropertyModel({
+      name: 'Pending Order',
+      count: data?.totalPendingOrders,
+    });
+    this.totalDispatchedOrders = new DashboardStatsPropertyModel({
+      name: 'Dispatched Orders',
+      count: data?.totalDispatchedOrders,
+    });
+    this.totalDeliveredOrders = new DashboardStatsPropertyModel({
+      name: 'Delivered Orders',
+      count: data?.totalDeliveredOrders,
+    });
+    this.totalReturnedOrders = new DashboardStatsPropertyModel({
+      name: 'Returned Orders',
+      count: data?.totalReturnedOrders,
+    });
+    this.repeatedCustomerPercentage = new DashboardStatsPropertyModel({
+      name: 'Repeated Customer',
+      count: data?.repeatedCustomerPercentage,
+    });
   }
 }
-
 
 export class DashboardStatsPropertyModel {
   name: string;
