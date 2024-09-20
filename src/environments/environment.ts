936b11e1b3f21d98b5936d4fdcf688ed8a207808
Fahad Subzwari <fahadsubzwari924@gmail.com>
2024-09-20 21:28:19 +0500

    cwc-68: fixed env path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:3000/api/v1',
+  // baseUrl: 'http://localhost:3000/api/v1',
   // baseUrl: 'http://13.233.95.165:3000/api/v1',
-  // baseUrl: 'https://cwc-nest.onrender.com/api/v1',
+  baseUrl: 'https://cwc-nest.onrender.com/api/v1',
 };
